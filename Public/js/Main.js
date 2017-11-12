f0d3e5cf3aacc1c101660acf2d6558a68b89c23d
Femi-lawal <33192240+Femi-lawal@users.noreply.github.com>
2017-11-12 21:29:45 +0100

    Add files via upload

--- Public/js/Main.js
+++ Public/js/Main.js
@@ -17,3 +17,23 @@ function deleteUser(){
 		return false;
 	}
 }
+
+$(document).ready(function(){
+	$('.findModifyUser').on('click', findModifyUser);
+});
+
+function findModifyUser(){
+	var confirmation = confirm('Edit/Update this user?');
+
+	if(confirmation){
+		$.ajax({
+			type:'UPDATE',
+			url: '/users/update/'+$(this).data('id')
+		}).done(function(response){
+			window.location.replace('/');
+		});
+		window.location.replace('/');
+	} else {
+		return false;
+	}
+} 
